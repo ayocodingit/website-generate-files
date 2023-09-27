af1c2638005a53887c05461712b2c438b642e2aa
ayocodingit <ayocodingit@gmail.com>
2023-09-27 15:38:30 +0700

    fix validate schema

--- src/transport/http/middleware/verifyAuth.ts
+++ src/transport/http/middleware/verifyAuth.ts
@@ -1,19 +1,29 @@
 import { NextFunction, Response } from 'express'
-import Jwt from '../../../pkg/jwt'
+import Error from '../../../pkg/error'
 import statusCode from '../../../pkg/statusCode'
+import Jwt from '../../../pkg/jwt'
 
-export const VerifyToken = (jwt: Jwt) => {
+export const VerifyAuth = (jwt: Jwt) => {
     return (req: any, res: Response, next: NextFunction) => {
-        const { token } = req.params
+        const { authorization } = req.headers
+
+        if (!authorization) {
+            return next(
+                new Error(
+                    statusCode.UNAUTHORIZED,
+                    statusCode[statusCode.UNAUTHORIZED]
+                )
+            )
+        }
+
+        const [_, token] = authorization.split('Bearer ')
 
         try {
             const decode = jwt.Verify(token)
             req['user'] = decode
             return next()
         } catch (err) {
-            return res.status(statusCode.UNAUTHORIZED).json({
-                error: statusCode[statusCode.UNAUTHORIZED],
-            })
+            return next(err)
         }
     }
 }
