924ce012db150a0baa107411a9688c77bf6da4b0
ayocodingit <ayocodingit@gmail.com>
2023-11-16 10:35:37 +0700

    add node scheduler

--- src/cron/removeFile.cron.ts
+++ src/cron/removeFile.cron.ts
@@ -1,11 +1,18 @@
 import fs from 'fs'
+import { scheduleJob } from 'node-schedule'
 
 const removeFile = (path: string, second: number) => {
-    setTimeout(() => {
-        if (fs.existsSync(path)) {
-            fs.unlinkSync(path)
+    const time = second * 1000
+    const startTime = new Date(Date.now() + time)
+    const endTime = new Date(startTime.getTime() + 1000)
+    scheduleJob(
+        { start: startTime, end: endTime, rule: '*/1 * * * * *' },
+        function () {
+            if (fs.existsSync(path)) {
+                fs.unlinkSync(path)
+            }
         }
-    }, second * 1000)
+    )
 }
 
 export default removeFile
