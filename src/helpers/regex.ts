c55068f60ea0aef53d6bede1feefeedb3ab81859
ayocodingit <ayocodingit@gmail.com>
2024-08-09 16:08:42 +0700

    add allow content type of pdf

--- src/helpers/regex.ts
+++ src/helpers/regex.ts
@@ -2,4 +2,4 @@ export const RegexContentTypeImage = /^image\//
 export const RegexContentTypeDoc =
     /vnd.openxmlformats-officedocument.wordprocessingml.document/
 export const RegexContentTypeAllow =
-    /vnd.openxmlformats-officedocument.wordprocessingml.document|image/
+    /vnd.openxmlformats-officedocument.wordprocessingml.document|image|application\/pdf/
